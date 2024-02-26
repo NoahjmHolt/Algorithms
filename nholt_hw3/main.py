b3ee0e04e86007178bf55bd3f9870374766525fd
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2024-02-26 11:53:18 -0700

    Bit more
    Almost done
    
    Just have the big function left
    
    Also a bit of a write up at the end but not too much

--- nholt_hw3/main.py
+++ nholt_hw3/main.py
@@ -14,16 +14,77 @@ import timeit
 # Knapsack Problem Solutions
 
 # exhaustive search
+def Exhaustive(val, weight, max):
+
+    return 0
 
 
 # greedy bugger
+def Greedy(val, weight, max):
+
+    grabHighest = []
+    for i in range(len(val)):
+        grabHighest.append(i)
+        if AddWeights(weight, grabHighest) > max:
+            grabHighest.pop()
+
+    return grabHighest
+
+
+# Other funcs
+# gets total value
+def AddValues(val, indexes):
+    total = 0
+
+    for i in indexes:
+        total += val[i]
+
+    return total
+
+
+# gets total weight
+def AddWeights(weight, indexes):
+    total = 0
+
+    for i in indexes:
+        total += weight[i]
+
+    return total
+
+
+# orders from most valuable down
+def OrderValues(val, weight):
+
+    for i in range(0, len(val)):
+        for j in range(i+1, len(val)):
+            if val[i] < val[j]:
+                tempVal = val[i]
+                tempWeight = weight[i]
+                val[i] = val[j]
+                weight[i] = weight[j]
+                val[j] = tempVal
+                weight[j] = tempWeight
+    # I looked and saw that this type of algorithm to sort is BigTheta(nlogn)
+    # As stated in hw doc, we will ignore when determining other times
 
 
 # main function
 maxWeigh = 10000
-values = random.randint(low=1, size=10)
+values = random.randint(100, size=10)
 weights = random.randint(low=1, high=10000, size=10)
 
-exhaustiveTime = 0
-greedyTime = 0
+exhaustiveStart = timeit.default_timer()
+
+exhaustiveEnd = timeit.default_timer()
+exhaustiveTime = round((exhaustiveEnd - exhaustiveStart) * 10 ** 6, 3)
+# returned nano so with difference return seconds
+
+# For greedy, ordering list first
+OrderValues(values, weights)
+greedyStart = timeit.default_timer()
+greedyIndexes = Greedy(values, weights, maxWeigh)
+greedyEnd = timeit.default_timer()
+greedyTime = round((greedyEnd - greedyStart) * 10 ** 6, 3)
+
+print(f"Greedy Time: {greedyTime} nano secs")
 
