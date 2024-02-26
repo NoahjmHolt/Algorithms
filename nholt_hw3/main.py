60a70aee415dec25b0fd6595b4e74012f8e4e82a
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2024-02-26 11:08:39 -0700

    begining coding portion
    Beginning coding portion of hw 3
    
    Trying to figure all the requirements and get them sorted out

---
+++ nholt_hw3/main.py
@@ -0,0 +1,29 @@
+'''
+
+Homework 3
+Noah Holt
+Algorithms
+
+Pseudo code provided in pdf.
+
+'''
+
+from numpy import random
+import timeit
+
+# Knapsack Problem Solutions
+
+# exhaustive search
+
+
+# greedy bugger
+
+
+# main function
+maxWeigh = 10000
+values = random.randint(low=1, size=10)
+weights = random.randint(low=1, high=10000, size=10)
+
+exhaustiveTime = 0
+greedyTime = 0
+
